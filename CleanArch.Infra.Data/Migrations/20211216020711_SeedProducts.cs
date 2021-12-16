12c24d80a30393e4ba1b55bec0aefc969d568d67
Denis Souza <denyssouza1@hotmail.com>
2021-12-15 23:13:55 -0300

    seed products

---
+++ CleanArch.Infra.Data/Migrations/20211216020711_SeedProducts.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArch.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql(@"INSERT INTO Products (Name, Description,  Price, Stock,Image, CategoryId)
+            VALUES ('Caderno Espiral', 'Caderno espiral 100 folhas', 7.45, 50,'caderno1.jpg',1)");
+            
+            mb.Sql(@"INSERT INTO Products (Name, Description,  Price, Stock,Image, CategoryId)
+            VALUES ('Estorno escolar', 'estojo escolar cinza', 7.45,70,'estojo1.jpg',1)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Products");
+        }
+    }
+}
